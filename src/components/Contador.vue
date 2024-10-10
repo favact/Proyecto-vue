3b45b978f8c368d8846e3df45110519b5ddadb43
Favio Massei <faviomassei@gmail.com>
2024-10-10 00:34:41 -0300

    Agregadas rutas

--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -1,8 +1,8 @@
 <template>
-<h1>Hola mundo</h1>
-<p>{{ contador }}</p>
-<button @click="incrementar">Incrementar</button>
-<button @click="decrementar">Decrementar</button>
+    <h1>Hola mundo</h1>
+    <p>{{ contador }}</p>
+    <button @click="incrementar">Incrementar</button>
+    <button @click="decrementar">Decrementar</button>
 </template>
 
 <script setup>
